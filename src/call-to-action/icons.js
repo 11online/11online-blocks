1cc2cb90b35ab7526bf3e27f0def29f0f33f35e8
Asya Nikitina <a.f.nikitina@gmail.com>
2018-11-21 13:48:36 -0700

    CREATED: created icons.js file that contains custom icons

--- src/call-to-action/icons.js
+++ src/call-to-action/icons.js
@@ -1,13 +1,13 @@
 const icons = {};
 
 icons.contrast = <svg width='20px' height='20px' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
-    <path d='m5 95h90v-90h-90zm10-70h10v-10h10v10h10v10h-10v10h-10v-10h-10zm70-10v70h-70z'
-    />
-    <path d='m45 65h30v10h-30z' />
+<path d='m5 95h90v-90h-90zm10-70h10v-10h10v10h10v10h-10v10h-10v-10h-10zm70-10v70h-70z'
+/>
+<path d='m45 65h30v10h-30z' />
 </svg>;
 
 icons.contrast2 = <svg id="Capa_1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
-viewBox="0 0 122 122">
+viewBox="0 0 100 100">
     <path d="M122,73.5v-46c0-11-9-20-20-20H20c-11,0-20,9-20,20v46c0,11,9,20,20,20h23.5l-10,10.6c-3.6,3.801-0.9,10.4,4.3,10.4H84 c5.3,0,8-6.5,4.3-10.4l-9.8-10.6H102C113,93.5,122,84.5,122,73.5z M109,75.7c0,3.3-2.7,5.8-6,5.8H67.2H54.8h-2c-2,0-3.6-0.7-4.6-2 c-1.8-2.2-2-5.7,0.4-8l50-50l0.2-0.2c3.7-3.8,10.2-1.1,10.2,4.2V75.7z"
     />
 </svg>;
