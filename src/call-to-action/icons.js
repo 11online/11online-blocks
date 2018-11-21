f5482dfea3d7a75819791e24007a11d582d38164
Asya Nikitina <a.f.nikitina@gmail.com>
2018-11-21 14:29:01 -0700

    UPDATED: removed the second SVG icon from icons.js -- didn't work

--- src/call-to-action/icons.js
+++ src/call-to-action/icons.js
@@ -6,10 +6,4 @@ icons.contrast = <svg width='20px' height='20px' viewBox='0 0 100 100' xmlns='ht
 <path d='m45 65h30v10h-30z' />
 </svg>;
 
-icons.contrast2 = <svg id="Capa_1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
-viewBox="0 0 100 100">
-    <path d="M122,73.5v-46c0-11-9-20-20-20H20c-11,0-20,9-20,20v46c0,11,9,20,20,20h23.5l-10,10.6c-3.6,3.801-0.9,10.4,4.3,10.4H84 c5.3,0,8-6.5,4.3-10.4l-9.8-10.6H102C113,93.5,122,84.5,122,73.5z M109,75.7c0,3.3-2.7,5.8-6,5.8H67.2H54.8h-2c-2,0-3.6-0.7-4.6-2 c-1.8-2.2-2-5.7,0.4-8l50-50l0.2-0.2c3.7-3.8,10.2-1.1,10.2,4.2V75.7z"
-    />
-</svg>;
-
 export default icons;
