39bc2fb29f609b2fb2e82afbab16469b152e5629
Asya Nikitina <a.f.nikitina@gmail.com>
2018-12-06 11:19:22 -0700

    UPDATED: modified the Inspector component -- added support for a new select control and changed inspector controls rendereing (call-to-action)

--- src/call-to-action/inspector.js
+++ src/call-to-action/inspector.js
@@ -27,21 +27,19 @@ export default class Inspector extends Component {
       const {
         attributes: {
             styleClass,
+            bgrOption,
             imgOpacity,
-            newTab,
             colorFontControl,
             colorBackgroundControl,
         },
         setAttributes
       } = this.props;
 
-      const toggleNewTab = () => setAttributes( { newTab: ! newTab } );
-
       return (
         <InspectorControls>
             <PanelBody>
                 <SelectControl
-                    label={ __("Style Option Control") }
+                    label={ __("Style Settings") }
                     help={ __("Select Style Option") }
                     value={ styleClass }
                     options={[
@@ -53,30 +51,21 @@ export default class Inspector extends Component {
             </PanelBody>
 
             <PanelBody>
-                <ToggleControl
-                    label={ __("Use Background Image?") }
-                    help={ __( bgrImg ? 'Use Background Image' : 'Use Background Color' ) }
-                    checked={ bgrImg }
-                    onChange={ toggleBgrImg }
-                />
-            </PanelBody>
-
-            <PanelBody>
-                <RangeControl
-                    beforeIcon="arrow-left-alt2"
-                    afterIcon="arrow-right-alt2"
-                    label={ __("Background Image Opacity" ) }
-                    help={ __("Use the slider to change the background image opacity on the scale from 1 to 10") }
-                    value={ imgOpacity }
-                    onChange={ imgOpacity => setAttributes( { imgOpacity } ) }
-                    initialPosition={ 10 }
-                    min={ 1 }
-                    max={ 10 }
+                <SelectControl
+                    label={ __("Background Settings") }
+                    help={ __("Select Background Option") }
+                    value={ bgrOption }
+                    options={[
+                        { value: "bgrImage", label: __("Background Image") },
+                        { value: "bgrColor", label: __("Background Color") },
+                        { value: "bgrNone", label: __("None") },
+                    ]}
+                    onChange={ bgrOption => setAttributes({ bgrOption }) }
                 />
             </PanelBody>
 
             <PanelColorSettings
-                title={__("Font Color Settings")}
+                title={__("Font Color")}
                 initialOpen={ false }
                 colorSettings={[
                     {
@@ -90,20 +79,38 @@ export default class Inspector extends Component {
                 ]}
             />
 
-            <PanelColorSettings
-                title={__("Background Color Settings")}
-                initialOpen={ false }
-                colorSettings={[
-                    {
-                        label: __("Background Color"),
-                        help: __("Select Background Color"),
-                        value: colorBackgroundControl,
-                        onChange: colorBackgroundControl => {
-                            setAttributes({ colorBackgroundControl });
-                        },
-                    }
-                ]}
-            />
+            { bgrOption === 'bgrImage' &&
+                <PanelBody>
+                    <RangeControl
+                        beforeIcon="arrow-left-alt2"
+                        afterIcon="arrow-right-alt2"
+                        label={ __("Background Image Opacity" ) }
+                        help={ __("Use the slider to change the background image opacity on the scale from 1 to 10") }
+                        value={ imgOpacity }
+                        onChange={ imgOpacity => setAttributes( { imgOpacity } ) }
+                        initialPosition={ 10 }
+                        min={ 1 }
+                        max={ 10 }
+                    />
+                </PanelBody>
+            }
+
+            { bgrOption === 'bgrColor' &&
+                <PanelColorSettings
+                    title={__("Background Color")}
+                    initialOpen={ false }
+                    colorSettings={[
+                        {
+                            label: __("Background Color"),
+                            help: __("Select Background Color"),
+                            value: colorBackgroundControl,
+                            onChange: colorBackgroundControl => {
+                                setAttributes({ colorBackgroundControl });
+                            },
+                        }
+                    ]}
+                />
+            }
         </InspectorControls>
       );
     }
