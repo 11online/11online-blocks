43c18a42c766bc2daa8739539cdbed2892982897
Asya Nikitina <a.f.nikitina@gmail.com>
2019-01-03 11:24:51 -0700

    UPDATED: added functionalities for the image upload/image remove and button add/button remove, tested (mycard)

--- src/mycard/card.js
+++ src/mycard/card.js
@@ -13,6 +13,10 @@ const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
 const { RichText } = wp.editor;
 const {
+    Tooltip,
+    TextControl,
+    Dropdown,
+    ToggleControl,
 } = wp.components;
 
 /**
@@ -30,10 +34,17 @@ export default class Card extends Component {
             attributes: {
                 cardTitle,
                 cardText,
+                cardTextAlignment,
+                cardImgID, 
+                cardImgURL, 
+                cardImgAlt,
+                cardBtnPresent,
+                cardBtnURL,
+                cardBtnText, 
+                newTab,  
             },
             className,
             setAttributes,
-            editable,
         } = this.props;
 
         const classes = classnames(
@@ -41,32 +52,107 @@ export default class Card extends Component {
             'mycard-eleven-online'
         );
 
+        const ButtonControls = (
+            <div className='button-box'>
+                <TextControl
+                    label={ __( 'Button Text' ) }
+                    value={ cardBtnText }
+                    onChange={ cardBtnText => setAttributes( { cardBtnText } ) }
+                />
+                <TextControl
+                    label={ __( 'Link URL' ) }
+                    value={ cardBtnURL }
+                    onChange={ cardBtnURL => setAttributes( { cardBtnURL } ) }
+                />
+                <ToggleControl
+                    label={ __("Open Link in New Tab?") }
+                    help={ __( newTab ? 'Open Link in a New Tab' : 'Open Link in the Same Window' ) }
+                    checked={ newTab }
+                    onChange={ () => setAttributes( { newTab: ! newTab } ) }
+                />
+            </div>
+		);
+
+        const renderButton = () => (
+            <div style={ { width: '100%' } }>
+                <Dropdown
+                    //position="bottom left"
+                    renderToggle={ ( { isOpen, onToggle } ) => (
+                        <div style={ { textAlign: cardTextAlignment } }>
+                            <a 
+                                className="button" 
+                                href="#0"
+                                onClick={ onToggle } 
+                                aria-expanded={ isOpen }
+                            >
+                                { cardBtnText }
+                            </a>
+                        </div>       
+                    ) }
+                    renderContent={ () => ButtonControls }
+                />
+            </div>
+        );
+
         return (
-            editable ?
+            this.props.editable ?
                 <div className={ classnames }>
                     <Fragment>
                         <Controls {...{ setAttributes, ...this.props }} />
-                        <RichText
-                            tagName="h2"
-                            placeholder={ __( 'Add your card title' ) }
-                            value={ cardTitle }
-                            // style={ { textAlign: textAlignment, color: colorFontControl } }
-                            onChange={ cardTitle  => setAttributes( { cardTitle  } ) }         
-                        />
-                        <RichText
-                            tagName="p"
-                            placeholder={ __( 'Add your card text' ) }
-                            value={ cardText }
-                            // style={ { textAlign: textAlignment, color: colorFontControl } }
-                            onChange={ cardText  => setAttributes( { cardText  } ) }                   		
-                        />
+                        { cardImgID && 
+                            <div className="img-mycard-eleven-online">
+                                <img
+                                    src={ cardImgURL }
+                                    alt={ cardImgAlt }
+                                />
+                            </div>
+                        }
+                        <div style={ { textAlign: cardTextAlignment, padding: '10px' } }>
+                            <RichText
+                                tagName="h2"
+                                placeholder={ __( 'Add your card title' ) }
+                                value={ cardTitle }
+                                // style={ { textAlign: textAlignment, color: colorFontControl } }
+                                onChange={ cardTitle  => setAttributes( { cardTitle  } ) }         
+                            />
+                            <RichText
+                                tagName="p"
+                                placeholder={ __( 'Add your card text' ) }
+                                value={ cardText }
+                                // style={ { textAlign: textAlignment, color: colorFontControl } }
+                                onChange={ cardText  => setAttributes( { cardText  } ) }                   		
+                            />
+                            { cardBtnPresent && 
+                                <div className="btn-mycard-eleven-online">
+                                    <Tooltip text={ __( 'Click to add or edit Button Text and Link URL' ) }>
+                                        { renderButton() }
+                                    </Tooltip> 
+                                </div>                      
+                            }
+                        </div>
+                        
                     </Fragment>
                 </div>
            :
                 <Fragment>
                     <div className={ classes }>
-                        <h2>{ cardTitle }</h2>
-                        <p>{ cardText }</p>
+                        { cardImgID && 
+                            <div className="img-mycard-eleven-online">
+                                <img
+                                    src={ cardImgURL }
+                                    alt={ cardImgAlt }
+                                />
+                            </div>
+                        }
+                        <div style={ { textAlign: cardTextAlignment, padding: '10px' } }>
+                            <h2>{ cardTitle }</h2>
+                            <p>{ cardText }</p>
+                            { cardBtnPresent && 
+                                <div className="btn-mycard-eleven-online">
+                                    { renderButton() }
+                                </div>                      
+                            }
+                        </div>   
                     </div>
                 </Fragment>
         );
