e0543b0bfdf4b6375ef705242c3ebaa12b1706d3
Asya Nikitina <a.f.nikitina@gmail.com>
2019-01-02 18:48:43 -0700

    UPDATED: had to change the query-type attributes to the simple ones (mycard)

--- src/mycard/attributes.js
+++ src/mycard/attributes.js
@@ -1,51 +1,56 @@
 const attributes = {
-    cardID: {
-        type: 'string',
-        source: "attribute",
-        selector: ".mycard-eleven-online",
-        attribute: "id",
-        default: '',
-    },
     cardTitle: {
         type: 'string',
+        source: 'html',
+        selector: 'h2',
         // source: 'text',
         // selector: '.card-title-eleven-online',
-        // default: 'Card Title',
+        default: 'Card Title',
     },
     cardText: {
         type: 'string',
         source: 'html',
         selector: 'p',
-        // default: 'Card text content',
+        default: 'Card text content',
     },
-    /*
-    card: {
-        type: 'object',
-        source: 'query',
-        selector: '.mycard-eleven-online',
-        query: {
-            // cardID: {
-            //     type: 'number',
-            //     // source: "attribute",
-            //     // attribute: "id",
-            //     default: 1,
-            // },
-            // cardTitle: {
-            //     type: 'string',
-            //     source: 'text',
-            //     selector: '.card-title-eleven-online',
-            //     default: 'Card Title',
-            // },
-            // cardText: {
-            //     type: 'string',
-            //     source: 'html',
-            //     selector: 'p',
-            //     default: 'Card text content',
-            // },
-        }
-
+    cardTextAlignment: {
+        type: 'string',
     },
-    */
+    cardImgID: {
+        type: 'number',
+        default: null,
+    },
+    cardImgURL: {
+        type: 'string',
+        // source: 'attribute',
+        // attribute: 'src',
+        // selector: 'img',
+        default: null,
+    },
+    cardImgAlt: {
+        type: 'string',
+        // source: 'attribute',
+        // attribute: 'alt',
+        // selector: 'img',
+        default: null,
+    },
+    cardBtnPresent: {
+        type: 'boolean',
+        default: false,
+    },
+    cardBtnURL: {
+        type: 'string',
+        source: 'attribute',
+        attribute: 'href',
+        selector: 'a',
+        default: 'http://'       
+    },
+    cardBtnText: {
+        type: 'string',
+        source: 'text',
+        selector: 'a',
+        default: 'Click here',
+    },
 }
 
 export default attributes;
