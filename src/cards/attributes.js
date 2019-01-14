2a16537f83001cc992a66b5e93b8687a1e233773
Asya Nikitina <a.f.nikitina@gmail.com>
2019-01-14 08:52:16 -0700

    UPDATED: rewrote the attributes completely for the purpose of using 'cards' as an array of objects (cards)

--- src/cards/attributes.js
+++ src/cards/attributes.js
@@ -2,86 +2,28 @@ const attributes = {
     columnClass: {
         type: 'string',
         default: 'one-half',
-    },
-    cardCount: {
-        type: 'number',
-        default: 1,
-    },
-    isEditing: {
-        type: 'boolean',
-        default: false,
-    },
-    currentID: {
-        type: 'string',
-        default: null,
-    },
+    },   
     cards: {
         type: 'array',
-        default: [],
-    },
-    cardID: {
-        type: 'string',
-        default: null,
-    },
-    cardTitle: {
-        type: 'string',
-        default: 'Card Title',
-    },
-    cardText: {
-        type: 'string',
-        default: 'Card text content',
-    },
-    cardTextAlignment: {
-        type: 'string',
-    },
-    cardHeadingSize: {
-        type: 'string',
-        default: 'h2',
-    },
-    cardImgID: {
-        type: 'number',
-        default: null,
-    },
-    cardImgURL: {
-        type: 'string',
-        default: null,
-    },
-    cardBtnPresent: {
-        type: 'boolean',
-        default: false,
-    },
-    buttonURL: {
-        type: 'string',
-        source: 'attribute',
-        attribute: 'href',
-        selector: 'a',
-        default: 'http://'       
-    },
-    buttonText: {
-        type: 'string',
-        source: 'text',
-        selector: 'a',
-        default: 'Click here',
-    },
-    buttonStyleClass: {
-        type: 'string',
-        default: 'primary',
-    },
-    newTab: {
-        type: 'boolean',
-        default: true,
-    },
-    useColor: {
-        type: 'boolean',
-        default: false,
-    },
-    colorFontControl: {
-        type: 'string',
-        default: '#000000'
-    },
-    colorBackgroundControl: {
-        type: 'string',
-        default: 'transparent',
+        default: [
+            {
+                isEditing: false,
+                cardTitle: 'Card Title',
+                cardText: 'Card text content',
+                cardTextAlignment: 'left',
+                cardHeadingSize: 'h2',
+                cardImgID: null,
+                cardImgURL: null,
+                cardBtnPresent: false,
+                buttonURL: 'http://',
+                buttonText: 'Click here',
+                buttonStyleClass: 'primary',
+                newTab: true,
+                useColor: false,
+                colorFontControl: '#000000',
+                colorBackgroundControl: '#FFFFFF',
+            },
+        ],
     },
 }
 
