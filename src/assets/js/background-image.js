3aa0d452edfa345aa91ed871b323d17b405fc811
Asya Nikitina <a.f.nikitina@gmail.com>
2019-01-17 15:56:52 -0700

    CREATED: created an external component (BackgroundImage) that allows uploading a new image and removing the existing one (background-image.js)

---
+++ src/assets/js/background-image.js
@@ -0,0 +1,84 @@
+/**
+ * Used Libraries
+ */
+const { __ } = wp.i18n;
+const { Component } = wp.element;
+const {
+    MediaUpload,
+    MediaUploadCheck,
+} = wp.editor;
+const {
+    Toolbar,
+    Button,
+    Tooltip,
+} = wp.components;
+
+/**
+ * Create a BackgroungImage Component
+ */
+export default class BackgroungImage extends Component {
+
+    constructor(props) {
+        super( ...arguments );
+        this.props = props;
+    }
+
+    render() {
+        const {
+            attributes: { 
+                imgID,
+                imgURL,
+             },
+            setAttributes
+          } = this.props;
+
+          const onSelectImage = img => {
+            setAttributes( {
+                imgID: img.id,
+                imgURL: img.url,
+            } );
+        };
+
+        const onRemoveImage = () => {
+            setAttributes({
+                imgID: null,
+                imgURL: null,
+            } );
+        };
+
+        return (
+            <Toolbar>
+                { imgID &&
+                    <Tooltip text={ __( 'Click to Remove Image' ) }>
+                    <Button
+                        className={ "components-button button button-large" }
+                        onClick={ onRemoveImage }
+                    >
+                        { __( 'Remove Image' ) }
+                    </Button>
+                </Tooltip>
+                }
+                { !imgID &&
+                    <MediaUploadCheck>
+                        <MediaUpload
+                            onSelect={ onSelectImage }
+                            type="image"
+                            value={ imgID }
+                            render={ ( { open } ) => (
+                                <Tooltip text={ __( 'Click to Upload Image' ) }>
+                                    <Button
+                                        className={ "components-button button button-large" }
+                                        onClick={ open }
+                                    >
+                                        { __( 'Upload Image' ) }
+                                    </Button>
+                                </Tooltip>
+                            ) }
+                        >
+                        </MediaUpload>
+                    </MediaUploadCheck>
+                }
+            </Toolbar>
+        );
+    }
+}
