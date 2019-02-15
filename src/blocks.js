9782cbf169769c89063a695c87f8356ef22d0fa3
Asya Nikitina <a.f.nikitina@gmail.com>
2019-02-15 11:30:08 -0700

    UPDATED: hooked a new block via the import statement in blocks.js

--- src/blocks.js
+++ src/blocks.js
@@ -9,16 +9,15 @@
  * Webpack is compiling as the input file.
  */
 
-// import './paragraph/block.js';
-// import './button/block.js';
-// import './checkerboard/block.js';
+import './paragraph/block.js';
+import './button/block.js';
+import './checkerboard/block.js';
 
 import './accordion/block.js';
-
-// import './call-to-action/block.js';
-// import './card/block.js';
-// import './cards/block.js';
-// import './new-cards/block.js';
-// import './container/block.js';
-// import './postcard/block.js';
+import './call-to-action/block.js';
+import './card/block.js';
+import './cards/block.js';
+import './new-cards/block.js';
+import './container/block.js';
+import './postcard/block.js';
 //import './paragraphs/block.js';
